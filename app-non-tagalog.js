9f0a426e06016f14e79d3d958c33916e07a4cd5e
allanzamora <67150355+allanzamora@users.noreply.github.com>
2026-09-09 18:25:11 +0800

    Add files via upload

---
+++ app-non-tagalog.js
@@ -0,0 +1,9 @@
+document.getElementById('content').innerHTML = `
+
+<center>
+<br><br><br><br><br><br><br><br><br>
+<h2>Undermaintenance</h2>
+<p>Please back after few hours.</p>
+</center>
+
+`;
---
+++ app-other.js
@@ -0,0 +1,13 @@
+document.getElementById('content').innerHTML = `
+
+document.getElementById('content').innerHTML = `
+
+<center>
+<br><br><br><br><br><br><br><br><br>
+<h2>Undermaintenance</h2>
+<p>Please back after few hours.</p>
+</center>
+
+`;
+
+`;
