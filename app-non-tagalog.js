a2eb6941557208404422a05e2350eeca2c614a99
allanzamora <67150355+allanzamora@users.noreply.github.com>
2026-09-10 01:31:42 +0800

    Add files via upload

--- app-non-tagalog.js
+++ app-non-tagalog.js
@@ -1,9 +1,56 @@
 document.getElementById('content').innerHTML = `
 
-<center>
-<br><br><br><br><br><br><br><br><br>
-<h2>Undermaintenance</h2>
-<p>Please back after few hours.</p>
-</center>
+  <ul>
+              <li><a href="aa/gg.html#sss=1a3B5-Xut8yxnviVhvzB7ECvpDzUYdfd_&tit=3+gatsu+no+Lion" target="_blank">3 gatsu no Lion</a></li>
+            <li><a href="aa/gg.html#sss=1OplThsHWlwmkbybBt7ipZwPsVRdovfuo&tit=3+Nen+D+Gumi+Glass+no+Kamen" target="_blank">3 Nen D Gumi Glass no Kamen</a></li>
+            <li><a href="aa/gg.html#sss=1FUhKhgRzEZ2Q29FKuemXXWwz6kORDsfc&tit=11+eyes" target="_blank">11 eyes</a></li>
+            <li><a href="aa/gg.html#sss=1_CXNjo9T1WOiXFqzYya1owplus5R2JeQ&tit=30+-+sai+no+Hoken+Taiiku" target="_blank">30 - sai no Hoken Taiiku</a></li>
+            <li><a href="aa/gg.html#sss=1u3UZj6oWBhjVGrgdwyYGk1GXtRcd0Mh5&tit=91+Days" target="_blank">91 Days</a></li>
+            <li><a href="aa/gg.html#sss=10mV55KSlgBB-8a00EJctPzF-UeioxUb4&tit=Absolute+Duo" target="_blank">Absolute Duo</a></li>
+            <li><a href="aa/gg.html#sss=1SbCxcNObGW5jN1ipawgR12ggpUNfOuxA&tit=ACCA+13+ku+Kansatsu+ka" target="_blank">ACCA 13 ku Kansatsu ka</a></li>
+            <li><a href="aa/gg.html#sss=1PKg5MT5AAWqZcSUITBxPzBLMbhP0ETqe&tit=Accel+World" target="_blank">Accel World</a></li>
+            <li><a href="aa/gg.html#sss=11wGR4Ot6ufKShhyKHVT0fCDSWWBENyLx&tit=Acchi+Kocchi" target="_blank">Acchi Kocchi</a></li>
+            <li><a href="aa/gg.html#sss=1UF54A8y9bPF8pLqKDnXu9WuaF0SQRjqe&tit=Ace+Attorney" target="_blank">Ace Attorney</a></li>
+            <li><a href="aa/gg.html#sss=1SDnBJPdcCCtOARve0kiu3ucUZq1vQfn4&tit=Ace+of+Diamond" target="_blank">Ace of Diamond</a></li>
+            <li><a href="aa/gg.html#sss=1MIrb6tNQCtb_b2UTURCf0rJKcHk6vn3t&tit=Active+Raid" target="_blank">Active Raid</a></li>
+            <li><a href="aa/gg.html#sss=10I-JlQs5QvFe9Brr4zKzZUKw6d41CnYN&tit=Air" target="_blank">Air</a></li>
+            <li><a href="aa/gg.html#sss=1_M8ETUFqDPQKN2TvJVxnX-PBr85VpMrN&tit=Air_Gear" target="_blank">Air_Gear</a></li>
+            <li><a href="aa/gg.html#sss=1LhrJnuCtHLXpAxrSNDAwrxc9DbO7W0IB&tit=Ai_Mai_Mi" target="_blank">Ai_Mai_Mi</a></li>
+            <li><a href="aa/gg.html#sss=1ZZGBnVLPyE0Fo9_tgUQE0DVTaU7eDoND&tit=Akagami_no_Shirayukihime" target="_blank">Akagami_no_Shirayukihime</a></li>
+            <li><a href="aa/gg.html#sss=1hEBCcOJ01eARWaj1aOAPrizFjoUgqJaE&tit=Akagi" target="_blank">Akagi</a></li>
+            <li><a href="aa/gg.html#sss=1JGM4KcH7NBXPjSvI4z_USo9KLfdDr6FF&tit=Akame_ga_Kill" target="_blank">Akame_ga_Kill</a></li>
+            <li><a href="aa/gg.html#sss=12j-uLjknxKuJe7bpX5PMbetLKYGIwdjE&tit=Akatsuki_no_Yona" target="_blank">Akatsuki_no_Yona</a></li>
+            <li><a href="aa/gg.html#sss=1T3DHDtBDjU6tZ5_cZ4nP1IhApbIiYrId&tit=AKB0048" target="_blank">AKB0048</a></li>
+            <li><a href="aa/gg.html#sss=1CoxzKvH3MnPWTIlevk3wrTEILOqup10R&tit=Akiba%27s_Trip_The_Animation" target="_blank">Akiba's_Trip_The_Animation</a></li>
+            <li><a href="aa/gg.html#sss=1dIDmHfCYy4ASwXw-IMqtostDLXU6hJ2k&tit=Aldnoah_Zero" target="_blank">Aldnoah_Zero</a></li>
+            <li><a href="aa/gg.html#sss=1xkC0KfY2Oi3HMGu0m6lzMu5qnqKrbbKF&tit=Alice_to_Zouroku" target="_blank">Alice_to_Zouroku</a></li>
+            <li><a href="aa/gg.html#sss=1pDkvd_3t6T-nOHQmk47OlJbDytdwlGjm&tit=All_Out" target="_blank">All_Out</a></li>
+            <li><a href="aa/gg.html#sss=1bktbF4bAtbh2z7myo_c9X36W1Rxp9aLD&tit=Amaama_to_Inazuma" target="_blank">Amaama_to_Inazuma</a></li>
+            <li><a href="aa/gg.html#sss=1QFO_i4o7r0iP2R323EqHDmCEiwnv5meC&tit=Amanchu" target="_blank">Amanchu</a></li>
+            <li><a href="aa/gg.html#sss=13zYoSWxQn8swLtveI1cVi9q7pZJO5iId&tit=Ame-iro_Cocoa" target="_blank">Ame-iro_Cocoa</a></li>
+            <li><a href="aa/gg.html#sss=1YUwPxOCr-gc82G8BeeihwLjuJUz54dLx&tit=AMNESIA" target="_blank">AMNESIA</a></li>
+            <li><a href="aa/gg.html#sss=11pJ_IrdCVzLDCv9OgTF4iFNrXezg4nvK&tit=Ange_Vierge" target="_blank">Ange_Vierge</a></li>
+            <li><a href="aa/gg.html#sss=1wvcgAtv22YG3adDeKpH6pIcAkctSoLdS&tit=Ani_Tore%21" target="_blank">Ani_Tore!</a></li>
+            <li><a href="aa/gg.html#sss=1K0w398HQTWVXR6QvYVomnHnrLRfOKRPy&tit=Anne_Happy" target="_blank">Anne_Happy</a></li>
+            <li><a href="aa/gg.html#sss=1MBwrjSMnbt33q8v0-YhPS7iTM91f2bL5&tit=Another" target="_blank">Another</a></li>
+            <li><a href="aa/gg.html#sss=19b2fQ7bz2xx0k3GQ6iZvrYUHwzYZjnGI&tit=Ano_Natsu_De_Matteru" target="_blank">Ano_Natsu_De_Matteru</a></li>
+            <li><a href="aa/gg.html#sss=1X5y1E7u4Cbn_sVEKAyjfRXdZFpvruVUR&tit=Ansatsu_Kyoushitsu" target="_blank">Ansatsu_Kyoushitsu</a></li>
+            <li><a href="aa/gg.html#sss=1Pd4AnTcwx2TRze4LdZMoGmrD_YPE68OQ&tit=Antique_Bakery" target="_blank">Antique_Bakery</a></li>
+            <li><a href="aa/gg.html#sss=1N7RUmDQ1AHXGmc8LjpJIKUumSr-rfyZ4&tit=Anyamal_Tantei_Kiruminzoo" target="_blank">Anyamal_Tantei_Kiruminzoo</a></li>
+            <li><a href="aa/gg.html#sss=1Yls25t-ersd6V0daBD5eDOgVJxGHIyhv&tit=Aoharu_x_Kikanjuu" target="_blank">Aoharu_x_Kikanjuu</a></li>
+            <li><a href="aa/gg.html#sss=1c4ajUYDTA5UpAJXYkKVykTBIJwxAGOGl&tit=Aoi_Bungaku" target="_blank">Aoi_Bungaku</a></li>
+            <li><a href="aa/gg.html#sss=1nzpPZVIYIYecn-zJY_f8oQl_8QHTrmID&tit=Aoki_Hagane_No_Arpeggio" target="_blank">Aoki_Hagane_No_Arpeggio</a></li>
+            <li><a href="aa/gg.html#sss=1Bdn4lUL79Ksp2igMHgM2TEdEcFaO7RJf&tit=Ao_no_Exorcist" target="_blank">Ao_no_Exorcist</a></li>
+            <li><a href="aa/gg.html#sss=1EsgCFLAGLTHktVacEmuw54XD3GWYPWoC&tit=Ao_no_Kanata_no_Four_Rhythm" target="_blank">Ao_no_Kanata_no_Four_Rhythm</a></li>
+            <li><a href="aa/gg.html#sss=1njMpXYQLAE6zotQCsCgPzJl1Y1tyt4LD&tit=Ao_Oni_The_Animation" target="_blank">Ao_Oni_The_Animation</a></li>
+            <li><a href="aa/gg.html#sss=1UqG0BDlf8_TtCEo14Ve5El5RcfevuWJL&tit=Aquarian_Logos" target="_blank">Aquarian_Logos</a></li>
+            <li><a href="aa/gg.html#sss=1TvJaLtEs0QehQ9yGl-a0Rnte1C8OcGEb&tit=Area_no_Kishi" target="_blank">Area_no_Kishi</a></li>
+            <li><a href="aa/gg.html#sss=1WVglWggcszzf10XozY_j9wzlzjDuvIq_&tit=Arslan_Senki" target="_blank">Arslan_Senki</a></li>
+            <li><a href="aa/gg.html#sss=1rQtsumn1a5usvkniYXmcd6Gdai-3N76p&tit=Asobi_ni_Iku_Yo%21" target="_blank">Asobi_ni_Iku_Yo!</a></li>
+            <li><a href="aa/gg.html#sss=1wWxupMOamiza3GPW70RcMR_TzwsaqOV3&tit=Astarotte_no_Omocha%21" target="_blank">Astarotte_no_Omocha!</a></li>
+            <li><a href="aa/gg.html#sss=1Pd1ljTySyQJ4yQhSgq6V5XyOMiioNLZ6&tit=Asura_Cryin" target="_blank">Asura_Cryin</a></li>
+            <li><a href="aa/gg.html#sss=14M6yCwE_To5d8SKCzXhPyT4lhK5yBO3n&tit=Atashinchi" target="_blank">Atashinchi</a></li>
+      </ul>
+
 
 `;
--- app-other.js
+++ app-other.js
@@ -1,10 +1,14 @@
 
 document.getElementById('content').innerHTML = `
 
-<center>
-<br><br><br><br><br><br><br><br><br>
-<h2>Undermaintenance</h2>
-<p>Please back after few hours.</p>
-</center>
+ <ul>
+        <li><a href="https://drive.google.com/file/d/1IA9bCLhyw-0ZW5zKq3NM-2DaEmNTdQXf/view?usp=drivesdk">3 Idiots Tagalog Dub</a></li>
+        
+        <li><a href="https://drive.google.com/file/d/1IX1mraQWLdCbB3KvjDcxdTnKHGtskteu/view?usp=drivesdk">The Driver</a></li>
+        
+        <li><a href="https://drive.google.com/file/d/1INZbJIefJynNGFs8dCWrEMTBlDH_Q3Qk/view?usp=drivesdk">The King of Gambling</a></li>
+        
+    
+</ul>
 
 `;
