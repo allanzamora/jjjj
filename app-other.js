cb68334177001887f36173ad920903af3af86262
allanzamora <67150355+allanzamora@users.noreply.github.com>
2026-09-11 02:25:28 +0800

    Add files via upload

--- app-other.js
+++ app-other.js
@@ -18,6 +18,56 @@ document.getElementById('content').innerHTML = `
         
         <li><a href="https://rumble.com/embed/v7b7c6o/">Secret: Untold Melody (2025) (UNCUT VERSION) (Tagalog Dubbed) (TMC DUB)</a></li>
         
+        <li><a href="https://rumble.com/embed/v7b6mey/">The Wig (2005) (Tagalog Dubbed) (GMA DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v7at7qi/">Papa Francisco AKA Francisco El Padre Jorge (2015) (UNCUT VERSION) (Tagalog Dubbed) (ABS-CBN DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v7aq4km/">The Impossible (2013) (Tagalog Dubbed) (GMA DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v7anxq6/">Spring Garden (2024) (UNCUT VERSION) (Tagalog Dubbed) (TMC DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v79rw9o/">Dark Tales Of Japan (2005) (UNCUT VERSION) (Tagalog Dubbed) (GMA DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v79jd7u/">DC League Of Super-Pets (2022) (UNCUT VERSION) (Tagalog Dubbed) (GMA DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v798y3e/">The Blue Elephant (2006) (Tagalog Dubbed) (GMA DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v798h4m/">Borderlands (2024) (UNCUT VERSION) (Tagalog Dubbed) (ABS-CBN DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v790hj2/">The Life Of Chuck (2025) (UNCUT VERSION) (Tagalog Dubbed) (ABS-CBN DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v783ghk/">The Angry Birds Movie 2 (2019) (Tagalog Dubbed) (UNCUT VERSION) (GMA DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v780s4y/">Knowing (2009) (Tagalog Dubbed) (UNCUT VERSION) (GMA DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v77omy6/">Coming Soon (2008) (Tagalog Dubbed) (GMA DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v77ldp0/">The Little Witch (2018) (UNCUT VERSION) (Tagalog Dubbed) (ABS-CBN DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v77ghbe/">Forgotten Evil (2017) (Tagalog Dubbed) (GMA DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v77clgk/">Crazy Romance (2019) (UNCUT VERSION) (Tagalog Dubbed) (ABS-CBN DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v76zl2a/">Lyle Lyle Crocodile (2022) (UNCUT VERSION) (Tagalog Dubbed) (GMA DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v76xmtm/">One Missed Call 1 (2003) (Tagalog Dubbed) (GMA DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v769n5i/">A Quiet Place (2018) (TV UNCUT VERSION) (Tagalog Dubbed) (GMA DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v764hge/">Paws Of Fury: The Legend Of Hank (2022) (TV UNCUT VERSION) (Tagalog Dubbed) (GMA DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v762lag/">Kiss And Cry (2017) (TV UNCUT VERSION) (Tagalog Dubbed) (GMA DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v7602ba/">Un Monde Merveilleux AKA Robot T-O (2024) (Tagalog Dubbed) (ABS-CBN DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v75yf12/">Conclave (2024) (Tagalog Dubbed) (ABS-CBN DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v75o8vs/">The Young Messiah (2016) (TV UNCUT VERSION) (Tagalog Dubbed) (ABS-CBN DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v75mq4c/">Operation Fortune Ruse De Guerre (2023) (Tagalog Dubbed) (ABS-CBN DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v74xb4a/">Lights Out (2016) (UNCUT VERSION) (Tagalog Dubbed) (GMA DUB)</a></li>
+        <!--
         <li><a href=""></a></li>
         
         <li><a href=""></a></li>
@@ -33,25 +83,8 @@ document.getElementById('content').innerHTML = `
         <li><a href=""></a></li>
         
         <li><a href=""></a></li>
-        
-        <li><a href=""></a></li>
-        
-        <li><a href=""></a></li>
-        
-        <li><a href=""></a></li>
-        
-        <li><a href=""></a></li>
-        
-        <li><a href=""></a></li>
-        
-        <li><a href=""></a></li>
-        
-        <li><a href=""></a></li>
-        
-        <li><a href=""></a></li>
-        
-        <li><a href=""></a></li>
-    
+    -->
 </ul>
 
 `;
+
