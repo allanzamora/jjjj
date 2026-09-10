ab686b46f2d99192f6f98a9db06be048cedab927
allanzamora <67150355+allanzamora@users.noreply.github.com>
2026-09-10 15:58:04 +0800

    Add files via upload

--- app-other.js
+++ app-other.js
@@ -2,13 +2,57 @@
 document.getElementById('content').innerHTML = `
 
  <ul>
-        <li><a href="https://drive.google.com/file/d/1IA9bCLhyw-0ZW5zKq3NM-2DaEmNTdQXf/view?usp=drivesdk">3 Idiots Tagalog Dub</a></li>
+        <li><a href="https://drive.google.com/file/d/1IA9bCLhyw-0ZW5zKq3NM-2DaEmNTdQXf/preview?usp=sharing">3 Idiots Tagalog Dub</a></li>
         
-        <li><a href="https://drive.google.com/file/d/1IX1mraQWLdCbB3KvjDcxdTnKHGtskteu/view?usp=drivesdk">The Driver</a></li>
+        <li><a href="https://drive.google.com/file/d/1IX1mraQWLdCbB3KvjDcxdTnKHGtskteu/preview?usp=sharing">The Driver Tagalog Dubbed</a></li>
         
-        <li><a href="https://drive.google.com/file/d/1INZbJIefJynNGFs8dCWrEMTBlDH_Q3Qk/view?usp=drivesdk">The King of Gambling</a></li>
+        <li><a href="https://drive.google.com/file/d/1INZbJIefJynNGFs8dCWrEMTBlDH_Q3Qk/preview?usp=sharing">The King of Gambling Tagalog Dubbed</a></li>
         
+        <li><a href="https://rumble.com/embed/v76m0lo/">Whitney (2015) (Tagalog Dubbed) (GMA DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v7cnrx8/">A Man Called Otto (2022) (Tagalog Dubbed) (GMA DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v7c2qf0/">Barbie Dreamtopia: Festival Of Fun (2017) (Tagalog Dubbed) (GMA DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v7begd4/">Barbie Dreamtopia (2016) (UNCUT VERSION) (Tagalog Dubbed) (GMA DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v7b7c6o/">Secret: Untold Melody (2025) (UNCUT VERSION) (Tagalog Dubbed) (TMC DUB)</a></li>
+        
+        <li><a href=""></a></li>
+        
+        <li><a href=""></a></li>
+        
+        <li><a href=""></a></li>
+        
+        <li><a href=""></a></li>
+        
+        <li><a href=""></a></li>
+        
+        <li><a href=""></a></li>
+        
+        <li><a href=""></a></li>
+        
+        <li><a href=""></a></li>
+        
+        <li><a href=""></a></li>
+        
+        <li><a href=""></a></li>
+        
+        <li><a href=""></a></li>
+        
+        <li><a href=""></a></li>
+        
+        <li><a href=""></a></li>
+        
+        <li><a href=""></a></li>
+        
+        <li><a href=""></a></li>
+        
+        <li><a href=""></a></li>
+        
+        <li><a href=""></a></li>
     
 </ul>
 
+`;
 `;
