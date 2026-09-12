e557181aa8e0b05f48db3aa94e03d5105b51fdea
allanzamora <67150355+allanzamora@users.noreply.github.com>
2026-09-12 23:34:42 +0800

    Add files via upload

--- app-other.js
+++ app-other.js
@@ -67,24 +67,146 @@ document.getElementById('content').innerHTML = `
         <li><a href="https://rumble.com/embed/v75mq4c/">Operation Fortune Ruse De Guerre (2023) (Tagalog Dubbed) (ABS-CBN DUB)</a></li>
         
         <li><a href="https://rumble.com/embed/v74xb4a/">Lights Out (2016) (UNCUT VERSION) (Tagalog Dubbed) (GMA DUB)</a></li>
-        <!--
-        <li><a href=""></a></li>
         
-        <li><a href=""></a></li>
+        <li><a href="https://rumble.com/embed/v74dsz2/">FLOAT (2023) (Tagalog Dubbed) (ABS-CBN DUB)</a></li>
         
-        <li><a href=""></a></li>
+        <li><a href="https://rumble.com/embed/v74b7si/">Nine Lives (2016) (Tagalog Dubbed) (TV5 DUB)</a></li>
         
-        <li><a href=""></a></li>
+        <li><a href="https://rumble.com/embed/v744fkq/">Kyouso No Musume AKA Master’s Daughter (2022) (Full Movie Episodes) (Tagalog Dubbed) (GMA DUB)</a></li>
         
-        <li><a href=""></a></li>
+        <li><a href="https://rumble.com/embed/v73zev2/">That Awkward Moment AKA Are We Officially Dating (2014) (Tagalog Dubbed) (ABS-CBN DUB)</a></li>
         
-        <li><a href=""></a></li>
+        <li><a href="https://rumble.com/embed/v73xk1a/">Darkness Of Man (2024) (Tagalog Dubbed) (TV5 DUB)</a></li>
         
-        <li><a href=""></a></li>
+        <li><a href="https://rumble.com/embed/v73or4a/">Maid In Manhattan (2002) (Tagalog Dubbed) (GMA DUB)</a></li>
         
-        <li><a href=""></a></li>
-    -->
+        <li><a href="https://rumble.com/embed/v73ad3i/">The Childe (2023) (Tagalog Dubbed) (UNCUT VERSION) (GMA DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v72vbwg/">Jurassic World Fallen Kingdom (2018) (Tagalog Dubbed) (UNCUT VERSION) (TV5 DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v72f08w/">Bring It On (2000) (Tagalog Dubbed) (GMA DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v725eic/">Picture Perfect Lies (2021) (Tagalog Dubbed) (GMA DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v724vhe/">Snowed In For Christmas (2021) (Tagalog Dubbed) (GMA DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v722ra6/">Snow White The Fairest Of Them All (2001) (Tagalog Dubbed) (GMA DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v71wd0k/">The Turning (2020) (Tagalog Dubbed) (GMA DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v71s55g/">The Bodyguard (1992) (Tagalog Dubbed) (GMA DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v71kyzo/">Cats And Peachtopia (2018) (Tagalog Dubbed) (GMA DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v71gwo2/">Braven (2018) (Tagalog Dubbed) (ABS-CBN DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v70tsb6/">Tremors 6: A Cold Day In Hell (2018) (Tagalog Dubbed) (TV5 DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v70tnya/">Mamma Mia The Movie (2008) (Tagalog Dubbed) (TV5 DUB) (TV UNCUT VERSION)</a></li>
+  
+        <li><a href="https://rumble.com/embed/v701fn6/">Rock Dog (2016) (Tagalog Dubbed) (TV5 DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v7013ye/">Skyscraper (2018) (Tagalog Dubbed) (TV5 DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v6zznt4/">Cop And A Half 2: New Recruit (2017) (Tagalog Dubbed) (TV5 DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v6zot2y/">The Land Before Time X The Great Longneck Migration (2003) (Tagalog Dubbed) (TV5 DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v6zlg8e/">Bring Me Home (2019) (Tagalog Dubbed) (TMC DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v6z9862/">Howling Village (2020) (Tagalog Dubbed) (TMC DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v6z7rjm/">The Conjuring (2013) (Tagalog Dubbed) (GMA DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v6x5c1s/">Malignant (2021) (Tagalog Dubbed) (GMA DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v6xjaps/">Death Forest 5 (2016) (Tagalog Dubbed) (TMC DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v6xdygq/">Death Forest 4 (2016) (Tagalog Dubbed) (TMC DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v6wwrq6/">Death Forest 3 (2015) (Tagalog Dubbed) (TMC DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v6wsk52/">Death Forest 2 (2015) (Tagalog Dubbed) (TMC DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v6wes3u/">Death Forest (2014) (Tagalog Dubbed) (TMC DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v6zn40e/">Kindergarten Cop 2 (2016) (Tagalog Dubbed) (TV5 DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v6w3uqq/">Kindergarten Cop (1990) (Tagalog Dubbed) (GMA DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v6vzp28/">Miss Baek (2018) (Tagalog Dubbed) (TVN Movies Pinoy)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v6vuang/">Kin (2018) (Tagalog Dubbed) (ABS-CBN DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v6vnf0s/">My Sister The Pig Lady (2015) (Tagalog Dubbed) (GMA DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v6vimgg/">The Mitchells Vs. The Machines (2021) (Tagalog Dubbed) (GMA DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v6vh6bk/">Mr. Zoo: The Missing Vip (2020) (Tagalog Dubbed) (TVN Movies Pinoy DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v6v5sk2/">Shaun The Sheep: The Movie (2015) (Tagalog Dubbed) (GMA DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v6usr4w/">Die Hexenprinzessin AKA The Witch Princess (2020) (Tagalog Dubbed) (GMA DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v6uqw3o/">The Land Before Time XI Invasion Of The Tinysauruses (2005) (Tagalog Dubbed) (TV5 DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v6ulvd4/">V.C. Andrews The Landry Series Part 2: Pearl In The Mist (2021) (Tagalog Dubbed) (GMA DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v6un376/">V.C. Andrews The Landry Series Part 4: Hidden Jewel (2021) (Tagalog Dubbed) (GMA DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v6uhnlq/">Anne Of Green Gables: The Good Stars (2017) (Tagalog Dubbed) (GMA DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v6udxmy/">An American Tail (1986) (Tagalog Dubbed) (GMA DUB) (Remastered Version)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v6udxim/">An American Tail (1986) (Tagalog Dubbed) (GMA DUB) (VHS Version)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v6uc5r0/">The Land Before Time (1988) (Tagalog Dubbed) (TV5 DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v6ubpdg/">Sadako 3D (2012) (Tagalog Dubbed) (GMA DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v6ub8zq/">Nocebo (2022) (Tagalog Dubbed) (GMA DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v6u8zlq/">My Little Pony: The Movie (2017) (Tagalog Dubbed) (ABS-CBN DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v6u50kk/">Hansel & Gretel: Witch Hunters (2013) (Tagalog Dubbed) (GMA DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v6txjx4/">Ringu 0: The Birthday (2000) (Tagalog Dubbed) (GMA DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v6tvrf8/">Dark Water (2002) (Tagalog Dubbed) (GMA DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v6tk5jt/">Step Up 3D (2010) (Tagalog Dubbed) (GMA DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v6tiemv/">High Strung 2: Freedance (2018) (Tagalog Dubbed) (ABS-CBN DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v6tf497/">Fight For The Token: The Secret Woman (2023) (Tagalog Dubbed) (GMA DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v6t8wch/">Train To Busan (2016) (Tagalog Dubbed) (TVN Movies Pinoy DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v6suasv/">About My Father (2023) (Tagalog Dubbed) (ABS-CBN DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v6sks59/">Twins (1988) (Tagalog Dubbed)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v6sgbzp/">Espectro AKA Demon Inside (2013) (Tagalog Dubbed) (ABS-CBN DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v6scc5b/">Step Up (2006) (Tagalog Dubbed)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v6sboif/">A-X-L (2018) (Tagalog Dubbed) (TMC DUB)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v6s9x9z/">Clifford The Big Red Dog (2021) (Tagalog Dubbed)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v6s6ynp/">+1 (Plus One) (2013) (Tagalog Dubbed)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v6s5auv/">Fantastic Mr. Fox (2009) (Tagalog Dubbed)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v6s56t9/">Trouble (2019) (Tagalog Dubbed)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v6s3xwh/">Boonie Bears: Back To Earth (2022) (Tagalog Dubbed)</a></li>
+        
+        <li><a href="https://rumble.com/embed/v6s2hv5/">The Con-Heartist (2020) (Tagalog Dubbed)</a></li>
+        
+
 </ul>
 
 `;
+
 
